aeccac17976df9de2e7c3a8f380ce89a8fa89cd7
devpatel <devpatel.dp.08@gmail.com>
2024-03-31 20:47:31 -0400

    A4

--- server.js
+++ server.js
@@ -6,7 +6,9 @@
 *
 * https://www.senecacollege.ca/about/policies/academic-integrity-policy.html
 *
-* Name: Devkumar Pankajbhai Patel Student ID: 171174212 Date: 19/03/2024
+* Name: Devkumar Pankajbhai Patel Student ID: 171174212 Date: 31/03/2024
+*
+* Published URL: ___________________________________________________________
 *
 ********************************************************************************/
 const express = require("express");
@@ -16,20 +18,19 @@ const app = express();
 const HTTP_PORT = process.env.PORT || 3000;
 
 app.use(express.static("public"));
+app.set('view engine', 'ejs');
 
 legoData
   .initialize()
   .then(() => {
-    app.listen(HTTP_PORT, () =>
-      console.log(`server listening on: http://localhost:${HTTP_PORT}`)
-    );
+    app.listen(HTTP_PORT, () => console.log(`server listening on: http://localhost:${HTTP_PORT}`));
 
     app.get("/", (req, res) => {
-      res.sendFile(path.join(__dirname, "views", "home.html"));
+      res.render("home");
     });
 
     app.get("/about", (req, res) => {
-      res.sendFile(path.join(__dirname, "views", "about.html"));
+      res.render("about");
     });
 
     app.get("/lego/sets", async (req, res) => {
@@ -57,7 +58,7 @@ legoData
     });
 
     app.use((req, res) => {
-      res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
+      res.status(404).render("404", { message: "Error! The page you're looking for does not exist." });
     });
   })
   .catch((error) => {
